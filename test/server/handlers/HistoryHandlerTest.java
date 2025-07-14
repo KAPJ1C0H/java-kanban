e7e41e83617357457eab832ca25d15aae7cd0f2d
KAPJ1C0H <polyakoff.art3m@ya.ru>
2025-07-14 11:26:32 +0300

    ТЗ 9

--- test/server/handlers/HistoryHandlerTest.java
+++ test/server/handlers/HistoryHandlerTest.java
@@ -7,6 +7,7 @@ import model.Task;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import server.HttpTaskServer;
 import service.FileBackedTaskManager;
 import service.Managers;
 
